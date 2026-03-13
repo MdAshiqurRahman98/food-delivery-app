8d8aac2d2254dda185731d74a9ae2c22210181aa
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-13 22:11:46 +0600

    Home updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,11 @@ import ItemSection from '../../components/items/ItemSection/ItemSection';
 
 const Home = () => {
     return (
-        <div className='bg-gray-100 min-h-screen'>
+        <>
             <Banner></Banner>
             <ItemSection title="Popular"></ItemSection>
             <ItemSection title="Recommended"></ItemSection>
-        </div>
+        </>
     );
 };
 
