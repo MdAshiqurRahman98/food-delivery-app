b845ffc0bdcfc8e8fe55eb36373e58d39593353d
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-13 21:55:51 +0600

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router';
 import router from './routes/Routes';
-import { ItemProvider } from './context/ItemContext';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ItemProvider>
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </ItemProvider>
+    </Provider>
   </StrictMode>,
 )
