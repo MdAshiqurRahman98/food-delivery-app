648db205a03389b35a7d1cf3973fbe78f9d79670
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-14 08:02:26 +0600

    ItemCard updated

--- src/components/items/ItemCard/ItemCard.jsx
+++ src/components/items/ItemCard/ItemCard.jsx
@@ -1,16 +1,16 @@
+
+
 const ItemCard = ({ item }) => {
     return (
-        <div className="bg-white rounded-2xl shadow-md overflow-hidden">
+        <div className="bg-white rounded-xl shadow-md overflow-hidden">
             <img
                 src={item.Image || item.image}
                 alt={item.Name}
                 className="h-40 w-full object-cover"
             />
 
-            <div className="py-2 text-center">
-                <h3 className="text-sm font-medium text-gray-700 px-2">
-                    {item.Name}
-                </h3>
+            <div className="p-2 text-center">
+                <h3 className="text-sm font-medium">{item.Name}</h3>
             </div>
         </div>
     );
