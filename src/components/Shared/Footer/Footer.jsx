eac69529fbec447b6ac65c4dc3f8bfc81ee3cec2
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-17 18:00:41 +0600

    Footer updated

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -3,41 +3,47 @@ import { FaGoogle, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="bg-orange-400 text-white mt-12 px-4 py-10">
+        <div className="bg-orange-400 text-white mt-12 px-4 py-10 flex flex-row justify-evenly">
+            <div>
+                {/* Subscribe */}
+                <div className="bg-white rounded-xl flex items-center justify-between px-4 py-2">
+                    <input
+                        type="email"
+                        placeholder="Enter Your Email"
+                        className="outline-none text-gray-600 w-full text-sm"
+                    />
+                    <button className="flex items-center gap-1 bg-orange-500 text-white text-sm px-7 py-2 rounded-xl font-semibold">
+                        Subscribe <FiArrowRight />
+                    </button>
+                </div>
 
-            {/* Subscribe */}
-            <div className="bg-white rounded-xl flex items-center justify-between px-4 py-3">
-                <input
-                    type="email"
-                    placeholder="Enter Your Email"
-                    className="outline-none text-gray-600 w-full text-sm"
-                />
-                <button className="flex items-center gap-1 bg-orange-500 text-white text-sm px-7 py-2 rounded-xl font-semibold">
-                    Subscribe <FiArrowRight />
-                </button>
+                <div className="flex flex-row justify-between gap-32">
+                    {/* Logo */}
+                    <div>
+                        <h2 className="text-3xl font-bold mt-8">
+                            pti<span className="text-orange-500">.</span>
+                        </h2>
+
+                        <p className="mt-4 text-sm">
+                            Copyright © Tripp. All Rights Reserved.
+                        </p>
+                    </div>
+
+                    {/* Social Icons */}
+                    <div className="flex justify-center gap-6 mt-16">
+                        <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
+                            <FaGoogle />
+                        </div>
+                        <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
+                            <FaTwitter />
+                        </div>
+                        <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
+                            <FaInstagram />
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            {/* Social Icons */}
-            <div className="flex justify-center gap-6 mt-8">
-                <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
-                    <FaGoogle />
-                </div>
-                <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
-                    <FaTwitter />
-                </div>
-                <div className="bg-orange-200 text-orange-500 p-3 rounded-full">
-                    <FaInstagram />
-                </div>
-            </div>
-
-            {/* Logo */}
-            <h2 className="text-center text-3xl font-bold mt-8">
-                pti<span className="text-pink-500">.</span>
-            </h2>
-
-            <p className="text-center mt-4 text-sm">
-                Copyright © Tripp. All Rights Reserved.
-            </p>
+            <img src="../../../../public/images/Image2.png" alt="Food basket" className="w-52 -mt-10" />
         </div>
     );
 };
